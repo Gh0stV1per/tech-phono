4fddff0d79403d3b4e5eb1157b7485e3a2917cd1
Gh0stV1per <bealert767@gmail.com>
2026-06-01 08:10:56 +0530

    router export fix final

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,12 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const getRouter = () => {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-  return createRouter({
+export const getRouter = () =>
+  createRouter({
     routeTree,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
-};
